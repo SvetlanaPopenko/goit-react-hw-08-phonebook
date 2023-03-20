681a8efd1afaa3f3dfd2758e1faf9fe2a38c2c1f
SvetlanaPopenko <spopenko02@gmail.com>
2023-03-20 15:11:53 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,31 @@
-import ContactForm from './ContactForm';
-import ContactList from './ContactList';
-import Filter from './Filter';
-import { Container } from './App.styled';
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { selectError, selectIsLoading } from 'redux/selectors';
-import { fetchContacts } from 'redux/operations';
-import { Toaster } from 'react-hot-toast';
+// import ContactForm from './ContactForm';
+// import ContactList from './ContactList';
+// import Filter from './Filter';
+// import { Container } from './App.styled';
+import { lazy } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { selectError, selectIsLoading } from 'redux/selectors';
+// import { fetchContacts } from 'redux/operations';
+// import { Toaster } from 'react-hot-toast';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { Layout } from './Layout/Layout';
+import { AppBar } from './AppBar/AppBar';
+
+const Home = lazy(() => import('../pages/Home'));
+const Login = lazy(() => import('../pages/Login'));
+const Register = lazy(() => import('../pages/Register'));
+const Contacts = lazy(() => import('../pages/Contacts'));
 
 export const App = () => {
-  const dispatch = useDispatch();
-  const isLoading = useSelector(selectIsLoading);
-  const error = useSelector(selectError);
+  // const dispatch = useDispatch();
+  // const isLoading = useSelector(selectIsLoading);
+  // const error = useSelector(selectError);
 
-  useEffect(() => {
-    dispatch(fetchContacts());
-  }, [dispatch]);
+  // useEffect(() => {
+  //   dispatch(fetchContacts());
+  // }, [dispatch]);
 
-  return (
-    <div
+  return (<div
       style={{
         marginTop: '50px',
         display: 'flex',
@@ -29,15 +36,26 @@ export const App = () => {
         color: '#eeedf8',
       }}
     >
-      <Container>
-        <h1>Phonebook</h1>
-        <ContactForm />
-        <h2>Contacts</h2>
-        <Filter />
-        <ContactList />
-        {isLoading && !error && <p>Loading...</p>}
-        <Toaster position="top-center" reverseOrder={false} />
-      </Container>
+<AppBar/>
+    <Routes>
+      <Route path="/" element={<Layout />} end>
+        <Route index element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />}/>
+        <Route path="/contacts" element={<Contacts />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Route>
+    </Routes>
     </div>
-  );
+    /* //   <Container>
+    //     <h1>Phonebook</h1>
+    //     <ContactForm />
+    //     <h2>Contacts</h2>
+    //     <Filter />
+    //     <ContactList />
+    //     {isLoading && !error && <p>Loading...</p>}
+    //     <Toaster position="top-center" reverseOrder={false} />
+    //   </Container> */
+    
+  )
 };
