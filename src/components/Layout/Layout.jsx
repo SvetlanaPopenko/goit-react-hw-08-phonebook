a4e951b268af93f6910ec4d429fec10dcf701688
SvetlanaPopenko <spopenko02@gmail.com>
2023-03-22 00:43:17 +0200

    Update Layout.jsx

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,3 +1,14 @@
+import { AppBar } from "components/AppBar/AppBar";
+import { Suspense } from "react";
+import { Toaster } from "react-hot-toast";
+import { Outlet } from "react-router-dom";
+
 export const Layout = () => {
-  return <div>Layout</div>;
+    return <div>
+        <AppBar/>
+        <Suspense fallback={null}>
+        <Outlet/>
+        </Suspense>
+        <Toaster position="top-right" reverseOrder={false}/>
+    </div>;
 };
