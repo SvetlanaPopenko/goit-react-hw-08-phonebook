ba1b92b016c024811fa5a0776f09f63bfc3ba71a
SvetlanaPopenko <spopenko02@gmail.com>
2023-03-22 00:43:19 +0200

    Update Navigation.jsx

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,5 +1,27 @@
+import { NavLink } from "react-router-dom"
+
+const styles = {
+    link: {
+  display: 'inline-block',
+  textDecoration: 'none',
+  padding: '12px',
+  fontWeight: 700,
+  color: '#2a363b',
+},
+
+activelink:{
+  color: '#e84a5f',
+}
+
+}
+
 export const Navigation = () => {
     return (
-        <div>Navigation</div>
+        <nav>
+            <NavLink to="/" exact style={styles.link} activeStyle={styles.activelink}>Home</NavLink>
+            <NavLink to="/contacts" exact style={styles.link} activeStyle={styles.activelink}>Contacts</NavLink>
+        </nav>
     )
 }
+
+export default Navigation;
