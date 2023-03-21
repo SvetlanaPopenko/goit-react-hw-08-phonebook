5be9c993ebad61e93e8da0a6d24c2bfee0232782
SvetlanaPopenko <spopenko02@gmail.com>
2023-03-22 00:43:10 +0200

    Update AppBar.jsx

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,3 +1,15 @@
+import { AuthNuv } from 'components/AuthNav/AuthNav';
+import { Navigation } from 'components/Navigation/Navigation';
+import { UserMenu } from 'components/UserMenu/UserMenu';
+import { useSelector } from 'react-redux';
+import { selectIsLoggedIn } from 'redux/auth/auth-selectors';
+
 export const AppBar = () => {
-  return <div>AppBar</div>;
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+  return (
+    <header>
+      <Navigation />
+      {isLoggedIn ? <UserMenu /> : <AuthNuv />}
+    </header>
+  );
 };
