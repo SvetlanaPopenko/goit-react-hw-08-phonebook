91b9836a9a8d1d724cabc4c1d2e571632b21c9a0
SvetlanaPopenko <spopenko02@gmail.com>
2023-03-22 00:43:08 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,15 @@
 // import ContactList from './ContactList';
 // import Filter from './Filter';
 // import { Container } from './App.styled';
-import { lazy } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
+import { lazy, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 // import { selectError, selectIsLoading } from 'redux/selectors';
 // import { fetchContacts } from 'redux/operations';
 // import { Toaster } from 'react-hot-toast';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { Layout } from './Layout/Layout';
-import { AppBar } from './AppBar/AppBar';
+import { useAuth } from 'hooks';
+import { refreshhUser } from 'redux/auth/auth-operations';
 
 const Home = lazy(() => import('../pages/Home'));
 const Login = lazy(() => import('../pages/Login'));
@@ -17,45 +18,48 @@ const Register = lazy(() => import('../pages/Register'));
 const Contacts = lazy(() => import('../pages/Contacts'));
 
 export const App = () => {
-  // const dispatch = useDispatch();
-  // const isLoading = useSelector(selectIsLoading);
-  // const error = useSelector(selectError);
+  const dispatch = useDispatch();
+  const { isRefreshing } = useAuth();
 
-  // useEffect(() => {
-  //   dispatch(fetchContacts());
-  // }, [dispatch]);
+  useEffect(() => {
+    dispatch(refreshhUser());
+  }, [dispatch]);
 
-  return (<div
-      style={{
-        marginTop: '50px',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'column',
-        fontSize: 40,
-        color: '#eeedf8',
-      }}
-    >
-<AppBar/>
+  return isRefreshing ? (
+    <b>Refreshing user...</b>
+  ) : (
     <Routes>
       <Route path="/" element={<Layout />} end>
         <Route index element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />}/>
+       
+          <Route path="/register" element={<Register />} />
+           <Route path="/login" element={<Login />} />
         <Route path="/contacts" element={<Contacts />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Route>
     </Routes>
-    </div>
-    /* //   <Container>
-    //     <h1>Phonebook</h1>
-    //     <ContactForm />
-    //     <h2>Contacts</h2>
-    //     <Filter />
-    //     <ContactList />
-    //     {isLoading && !error && <p>Loading...</p>}
-    //     <Toaster position="top-center" reverseOrder={false} />
-    //   </Container> */
-    
-  )
+  );
+
+  //   return (<div
+  //       style={{
+  //         marginTop: '50px',
+  //         display: 'flex',
+  //         justifyContent: 'center',
+  //         alignItems: 'center',
+  //         flexDirection: 'column',
+  //         fontSize: 40,
+  //         color: '#eeedf8',
+  //       }}
+  //     >
+  // <AppBar/>
+  //     <Routes>
+  //       <Route path="/" element={<Layout />} end>
+  //         <Route index element={<Home />} />
+  //         <Route path="/login" element={<Login />} />
+  //         <Route path="/register" element={<Register />}/>
+  //         <Route path="/contacts" element={<Contacts />} />
+  //         <Route path="*" element={<Navigate to="/" replace />} />
+  //       </Route>
+  //     </Routes>
+  //     </div>
 };
