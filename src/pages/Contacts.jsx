1c58a0a4250b5eae5b260773598bd8c55198598a
SvetlanaPopenko <spopenko02@gmail.com>
2023-03-23 02:57:19 +0200

    Update Contacts.jsx

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,5 +1,25 @@
+import ContactForm from 'components/ContactForm';
+import ContactList from 'components/ContactList';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchContacts } from 'redux/operations';
+import { selectIsLoading } from 'redux/selectors';
+
 const Contacts = () => {
-  return <div>Contacts</div>;
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+  return (
+    <>
+      <h1>Contacts</h1>
+      <ContactForm />
+      <div>{isLoading && 'Request in progress...'}</div>
+      <ContactList />
+    </>
+  );
 };
 
 export default Contacts;
