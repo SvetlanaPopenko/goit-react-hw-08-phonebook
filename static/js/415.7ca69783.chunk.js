4798e375d9b31cee1c0c44328ff38dba75ffd39b
SvetlanaPopenko <105171668+SvetlanaPopenko@users.noreply.github.com>
2023-03-23 00:58:33 +0000

    Deploying to gh-pages from @ SvetlanaPopenko/goit-react-hw-08-phonebook@985e7393f30b8d08b4dbb77a36df02f17b2f7a94 🚀

---
+++ static/js/415.7ca69783.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{5415:function(e,t,o){o.r(t);var r=o(184);t.default=function(){return(0,r.jsx)("div",{children:(0,r.jsx)("h1",{children:"Welcome to your Phonebook"})})}}}]);
+//# sourceMappingURL=415.7ca69783.chunk.js.map
