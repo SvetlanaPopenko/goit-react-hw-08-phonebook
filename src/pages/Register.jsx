985e7393f30b8d08b4dbb77a36df02f17b2f7a94
SvetlanaPopenko <spopenko02@gmail.com>
2023-03-23 02:57:26 +0200

    Update Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,71 +1,10 @@
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { register } from '../redux/auth/auth-operations';
-
-const styles = {
-  form: {
-    width: 320,
-  },
-  label: {
-    display: 'flex',
-    flexDirection: 'column',
-    marginBottom: 15,
-  },
-};
+import { RegisterForm } from 'components/RegisterForm/RegisterForm';
 
 export const Register = () => {
-  const dispatch = useDispatch();
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleChange = ({ target: { name, value } }) => {
-    switch (name) {
-      case 'name':
-        return setName(value);
-      case 'email':
-        return setEmail(value);
-      case 'password':
-        return setPassword(value);
-      default:
-        return;
-    }
-  };
-
-  const handleSubmit = evt => {
-    evt.preventDefault();
-    dispatch(register({ name, email, password }));
-    setName('');
-    setEmail('');
-    setPassword('');
-  };
-  return (
+     return (
     <div>
       <h1>Register</h1>
-      <form onSubmit={handleSubmit} style={styles.form} autoComplete="off">
-        <label style={styles.label}>
-          Name
-          <input type="text" name="name" value={name} onChange={handleChange} />
-        </label>
-        <label style={styles.label}>
-          Email
-          <input
-            type="text"
-            name="email"
-            value={email}
-            onChange={handleChange}
-          />
-              </label>
-              <label style={styles.label}>
-          Password
-          <input
-            type="text"
-            name="passwrd"
-            value={password}
-            onChange={handleChange}
-          />
-        </label>
-      </form>
+      <RegisterForm/>
     </div>
   );
 };
