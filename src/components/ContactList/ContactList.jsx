78b4f2bd899e6ace9f41f4e764300b39cd738393
SvetlanaPopenko <spopenko02@gmail.com>
2023-03-24 15:00:43 +0200

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,11 +1,18 @@
 import ContactItem from 'components/ContactItem';
 import PropTypes from 'prop-types';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { ContactsList, ContactItems } from './ContactList.styled';
 import { selectVisibleContacts } from 'redux/contacts/selectors';
+import { useEffect } from 'react';
+import { fetchContacts } from 'redux/contacts/operations';
 
 const ContactList = () => {
   const visibleContacts = useSelector(selectVisibleContacts);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
 
   return (
     <>
