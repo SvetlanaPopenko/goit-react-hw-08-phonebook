cf18754baf681022a998a5a505f9d99fa320fc46
SvetlanaPopenko <spopenko02@gmail.com>
2023-03-24 18:24:14 +0200

    Update services.js

--- src/components/services.js
+++ src/components/services.js
@@ -2,5 +2,5 @@ export const checkContact = (contacts, values) => {
   return contacts.find(contact => contact.name === values.name.trim());
 };
 export const checkNumber = (contacts, values) => {
-  return contacts.find(contact => contact.phone === values.phone.trim());
+  return contacts.find(contact => contact.number === values.number.trim());
 };
