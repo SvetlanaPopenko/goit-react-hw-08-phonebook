d83fc32062c1dd35a5eb69b7e91820432e0d1d67
SvetlanaPopenko <spopenko02@gmail.com>
2023-03-24 04:40:49 +0200

    Update AppBar.jsx

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -5,7 +5,7 @@ import { useAuth } from 'hooks';
 
 
 export const AppBar = () => {
-  const isLoggedIn = useAuth();
+  const {isLoggedIn} = useAuth();
   return (
     <header>
       <Navigation />
